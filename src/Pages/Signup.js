5634eaf3df09542e28a6c5972bb06d9e39ba924e
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2024-07-27 14:22:05 +0530

    new updates

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './login.css';
+import './Signup.css';
 import logo from '../assets/susl_logo_transparent1.png';
 import NavBar from "./Components/NavBar";
 import { useNavigate, Link } from "react-router-dom";
@@ -19,35 +19,36 @@ const Signup = () => {
                         <img src={logo} alt="SUSL Logo" />
                     </div>
                     <div className="loginCenter">
-                        <h1 style={{fontSize:'60px'}}>Create an Account</h1>
-                        <div className="loginForm" style={{maxWidth:'550px'}}>
-                            <div className="loginFormLabel" style={{display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
-                                <div className="signUpPassword">
-                                    <label>Username</label>
-                                    <input className="loginInput" placeholder="enter your username" /></div>
-                                <div className="signUpPassword">    
-                                <label>Email</label>
-                                <input className="loginInput" placeholder="enter your email" /></div>
+                        <h1>Create an Account</h1>
+                        <div className="signupForm">
+                            <div className="loginFormLabel">
+                            <div className="signUpPassword">
+                                <label>Username</label>
+                                <input className="loginInput" placeholder="enter your username" />
                             </div>
-                            <div className="loginFormLabel1" style={{display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
-                                <div className="signUpPassword" style={{display:'flex',flexDirection:'row', alignItems:'flex-start'}}>
-                                   <label>Password</label>
-                                   <input className="loginInput" placeholder="enter your password" type="password" />   
-                                </div>
-                                <div className="signUpPassword">
-                                  <label>Confirm Password</label>
-                                  <input className="loginInput" placeholder="confirm your password" type="password" />  
-                                </div>
-                                
+                            <div className="signUpPassword">
+                                <label>Email</label>
+                                <input className="loginInput" placeholder="enter your email" />
+                            </div>
+                            </div>
+                            <div className="loginFormLabel1">
+                            <div className="signUpPassword">
+                                <label>Password</label>
+                                <input className="loginInput" placeholder="enter your password" type="password" />
+                            </div>
+                            <div className="signUpPassword">
+                                <label>Confirm Password</label>
+                                <input className="loginInput" placeholder="confirm your password" type="password" />
+                            </div>
                             </div>
                             <label>
-                                Or Already have an account, Login <span style={{ color: '#FF6B00', cursor: 'pointer' }} onClick={handleSignupClick}>here</span>
+                            Or Already have an account, Login <span onClick={handleSignupClick}>here</span>
                             </label>
                             <button className="login_home">
-                                Sign Up
+                            Sign Up
                             </button>
                         </div>
-                    </div>
+                        </div>
                 </div>
             </div>
         </div>
